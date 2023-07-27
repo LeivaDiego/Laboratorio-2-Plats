928ac8043c8539fa70c07a9dcb981cf41df467ce
LeivaDiego <lei21752@uvg.edu.gt>
2023-07-26 22:57:57 -0600

    v2.1 Buscar Perfil
    Funcion de busqueda de perfil agregada

--- Lab2/src/main/kotlin/Main.kt
+++ Lab2/src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@
  * @author diego leiva
  * */
 fun main() {
+
+
     //Perfiles precargados
     var users: MutableList<PerfilUsuario> = mutableListOf()
     users.add(PerfilUsuario(1,"Diego","Leiva","thisurl.com",22,"lei21752@uvg.edu.gt","esta es la bio", state = State.Activo))
@@ -22,16 +24,16 @@ fun main() {
 
         when(readln()){
             "1" -> {
-                println("Eligio 1")
+                newProfile(users)
             }
             "2" -> {
-                println("Eligio 2")
+                searchProfile(users)
             }
             "3" -> {
-                println("Eligio 3")
+                deleteProfile(users)
             }
             "4" -> {
-                println("Eligio 4")
+                addHobby(users)
             }
             "5" -> {
                 println("Ha salido del programa")
@@ -44,3 +46,58 @@ fun main() {
     }
 }
 
+
+/**
+ * Funcion que solicita la informacion del usuario y la agrega a los perfiles
+ * @param profiles, el listado con todos los perfiles de usuario
+ */
+fun newProfile(profiles: MutableList<PerfilUsuario>) {
+
+}
+
+
+/**
+ * Funcion que busca un perfil de usuario por su ID y muestra en consola toda la informacion de dicho usuario
+ * @param profiles, el listado con todos los perfiles de usuario
+ */
+fun searchProfile(profiles: MutableList<PerfilUsuario>) {
+    println("[Buscar perfil]")
+    print("Ingresa ID o el nombre y apellido: ")
+    val search = readlnOrNull()
+
+    // Intenta convertir el valor de entrada del usuario a un numero entero
+    // Si la conversion tiene éxito, significa que el usuario ingreso un entero, y filtra la lista de perfiles para
+    // encontrar aquellos cuyo ID coincide con el numero ingresado.
+    // Si la conversion falla y se devuelve null, significa que el usuario ingreso una cadena de texto, y filtra la
+    // lista de perfiles para encontrar aquellos cuyo nombre y apellido coinciden con la cadena de texto ingresada
+    val results = when (val toIntOrNull = search?.toIntOrNull()) {
+        null -> profiles.filter { (it.name + " " + it.lastname) == search }
+        else -> profiles.filter { it.id == toIntOrNull }
+    }
+
+    //Verificamos si encontro o no el perfil
+    if (results.isEmpty()) {
+        println("No se encontró el perfil")
+    } else {
+        for (user in results) {
+            println("ID: ${user.id}, Nombre: ${user.name}, Apellido: ${user.lastname}, URL: ${user.url}, Edad: ${user.age}, Correo: ${user.email}, Biografia: ${user.bio}, Estado: ${user.state}, Hobbies: ${user.hobbies.joinToString(", ") { it.title }}")
+        }
+    }
+}
+
+
+/**
+ * Funcion que elimina un perfil de usuario especifico por ID
+ * @param profiles, el listado con todos los perfiles de usuario
+ */
+fun deleteProfile(profiles: MutableList<PerfilUsuario>) {
+
+}
+
+/**
+ * Funcion que agrega un hobby nuevo a un perfil de usuario en especifico
+ * @param profiles, el listado con todos los perfiles de usuario
+ */
+fun addHobby(profiles: MutableList<PerfilUsuario>) {
+
+}
